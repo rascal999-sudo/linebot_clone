662d9ea4038917920304cc68b1b04f985e8dc493
rascal999-sudo <75905681+rascal999-sudo@users.noreply.github.com>
2021-01-21 23:01:45 +0900

    Update demo.js

--- examples/demo.js
+++ examples/demo.js
@@ -40,7 +40,7 @@ bot.on('message', function (event) {
           });
           break;
         case 'Push':
-          bot.push(event.destination, event.massage.data);
+          bot.push(event.destination, event.message.data);
           break;
         case 'Push2':
           bot.push('Cba71ba25dafbd6a1472c655fe22979e2', 'Push to group');
